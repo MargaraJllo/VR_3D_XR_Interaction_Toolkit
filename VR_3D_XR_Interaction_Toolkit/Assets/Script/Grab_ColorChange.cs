307276d50fee6d087601a89f41e2f98eba4defaf
LuisioFake <87985208+LuisioFake@users.noreply.github.com>
2022-02-18 15:29:47 -0500

    Final de la clase
    :)

---
+++ VR_3D_XR_Interaction_Toolkit/Assets/Script/Grab_ColorChange.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Grab_ColorChange : MonoBehaviour
+{
+
+    public Material GreenColor =null;
+    public Material PurpleColor = null;
+    private MeshRenderer meshRenderer = null;
+    private XRGrabInteractable grabInteractable = null;
+    // Start is called before the first frame update
+
+    void Awake(){
+        gameObject.GetComponent<MeshRenderer>();
+
+    }
+
+    public void SetColor(Material Color)
+    {
+        meshRenderer.material = Color;
+    }
+
+ 
+ }
